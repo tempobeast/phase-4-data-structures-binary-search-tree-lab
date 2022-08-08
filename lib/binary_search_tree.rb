908eb7646a2eb529c423b42ced5f73d3ba8effe0
Zach <shaw.zacht@gmail.com>
2022-08-08 16:08:55 -0700

    commit tree 6ed0bb80bc9d2a34d470eaf32040759bee1d2912

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,46 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    node = @root
+
+    if node.nil?
+      return nil
+    else
+      while node
+        if node.value == value
+          return node
+        else 
+          if value > node.value
+            node = node.right
+          else
+            node = node.left
+          end
+        end
+      end
+    end
   end
 
   def insert(value)
-    # your code here
+    node = @root
+    new_node = Node.new(value)
+    if node.nil?
+      self.root = new_node
+    end
+    while node
+        if new_node.value > node.value && node.right.nil?
+          new_node = node.right
+          return new_node
+        elsif value < node.value && node.left.nil?
+          new_node = node.left
+          return new_node
+        elsif value > node.value && node.right
+          node = node.right
+        else
+          node = node.left
+        end
+        return nil
+    end  
+    return new_node
   end
 
 end 
